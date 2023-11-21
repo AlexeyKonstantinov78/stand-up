66a823883130f40eda3bc452a087f1dd60a9f6ef
Aleksey Konstantinov Ubunty <alex_kav@mail.ru>
2023-11-21 10:34:29 +0300

    чтение файла промис

--- api-standup/index.js
+++ api-standup/index.js
@@ -1,13 +1,19 @@
 import http from 'node:http';
+import fs from 'node:fs/promises';
+
+const data = await fs.readFile('package.json', "utf-8");
+console.log('data: ', data);
+
 
 const PORT = 8080;
 
 http
   .createServer((req, res) => {
     res.writeHead(200, {      
-      "Content-Type": "text/plain; charset=utf-8",
+      "Content-Type": "text/html; charset=utf-8",
+      "Access-Control-Allow-Origin": '*',
     });
-    res.end("Привет мир");
+    res.end("<h1>Привет мир</h1>");
 
   })
   .listen(PORT);
