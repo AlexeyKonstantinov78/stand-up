489d60d6111153386962b41ad43de6dec19fed7e
Aleksey Konstantinov Ubunty <alex_kav@mail.ru>
2023-11-22 11:22:14 +0300

    запуск севрера в одтельную функцию проверка на наличемк файлов и создание

--- api-standup/index.js
+++ api-standup/index.js
@@ -2,33 +2,60 @@ import http from 'node:http';
 import fs from 'node:fs/promises';
 
 const PORT = 8080;
-const fileName = 'comedians.json';
+const COMEDIANS = './comedians.json';
+const CLIENTS = './clients.json';
 
-http
-  .createServer(async (req, res) => {  
-    if (req.method === 'GET' && req.url === '/comedians') {
-      try {
-        const data = await fs.readFile(fileName, 'utf-8');
-        
-        res.writeHead(200, {      
-          "Content-Type": "application/json; charset=utf-8",
+const checkFiles = async () => {
+  try {
+    await fs.access(COMEDIANS);
+  } catch (error) {
+    console.error(`File ${COMEDIANS} not fond`);
+    return false;  
+  }
+
+  try {
+    await fs.access(CLIENTS);
+  } catch (error) {
+    await fs.writeFile(CLIENTS, JSON.stringify([]));
+    console.error(`Файл ${CLIENTS} был создан`);
+    return false;
+  }
+
+  return true;
+};
+
+const startServer = async () => {
+
+  if (!(await checkFiles())) return;
+
+  http
+    .createServer(async (req, res) => {  
+      if (req.method === 'GET' && req.url === '/comedians') {
+        try {
+          const data = await fs.readFile(COMEDIANS, 'utf-8');
+          
+          res.writeHead(200, {      
+            "Content-Type": "application/json; charset=utf-8",
+            "Access-Control-Allow-Origin": '*',
+          });
+          res.end(data);
+        } catch (error) {
+          res.writeHead('500', { 
+            "Content-Type": "text/plain; charset=utf-8", 
+          });
+          res.end(`Ошибка сервера: ${error.message}`);
+        }
+      } else {
+        res.writeHead(404, {
+          "Content-Type": "text/html; charset=utf-8",
           "Access-Control-Allow-Origin": '*',
         });
-        res.end(data);
-      } catch (error) {
-        res.writeHead('500', { 
-          "Content-Type": "text/plain; charset=utf-8", 
-        });
-        res.end(`Ошибка сервера: ${error.message}`);
+        res.end("<h2>Not found</h2>");
       }
-    } else {
-      res.writeHead(404, {
-        "Content-Type": "text/html; charset=utf-8",
-        "Access-Control-Allow-Origin": '*',
-      });
-      res.end("<h2>Not found</h2>");
-    }
-  })
-  .listen(PORT);
+    })
+    .listen(PORT);
+  
+  console.log(`Сервер запущен http://localhost:${PORT}`);
+};
 
-console.log(`Сервер запущен http://localhost:${PORT}`);
+startServer();
